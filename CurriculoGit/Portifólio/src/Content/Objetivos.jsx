46d967f81a5c3e508c76b5fba664050eff0a5f19
Leandro-a11 <letalinkpark@gmail.com>
2022-06-07 14:00:32 -0300

    Atualizado Com Sucesso

--- CurriculoGit/Portifólio/src/Content/Objetivos.jsx
+++ CurriculoGit/Portifólio/src/Content/Objetivos.jsx
@@ -8,7 +8,7 @@ export default function TextoObjetivos() {
                     <h2>Estou Intrigado e Dedicado Com Essa Área, Desde o Meu Primeiro Contato Com o Famoso "Olá Mundo"</h2>
                     <h2>Estou Decidido A Aprender Mais e Mais, Sempre Visando O Crescimento Pessoal,Profissional e o Da Minha Empresa</h2>
                     <h1>Tenho Qualidades Como:</h1>
-                    <h2><q>Responsabilidade,Dedicação, Interesse em Aprender, Honestidade, </q></h2>
+                    <h2><q>Responsabilidade,Dedicação, Interesse em Aprender, Honestidade,</q></h2>
                 </div>
             </div>
         </div>
