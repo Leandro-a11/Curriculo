ba93828df9af79c26ec405ec103f1375a17eed7b
Leandro-a11 <letalinkpark@gmail.com>
2022-06-08 17:21:54 -0300

    Trocada a Foto

--- CurriculoGit/Portifólio/src/Content/Visaogeral.jsx
+++ CurriculoGit/Portifólio/src/Content/Visaogeral.jsx
@@ -1,4 +1,4 @@
-import FotoPerfil from '../assets/LogoWraith.png'
+import FotoPerfil from '../assets/FotoDePerfil.jpeg'
 import Image from 'next/image'
 import style from '../styles/Content.module.css'
 export default function TextoVisaoGeral() {
@@ -8,13 +8,15 @@ export default function TextoVisaoGeral() {
                 <div>
                     <h1>Meu nome é Leandro Rodrigues Da Silva</h1>
                     <h2>18 Anos , Brasileiro , Solteiro , Minas Gerais-Belo Horizonte </h2>
-                    <h1>Atualmente</h1><h2>Estou Estudando Programação ,Mais Precisamente Web Desenvolvimento </h2>
+                    <h1>Atualmente</h1>
+                    <h2>Estou Estudando Programação ,Mais Precisamente Web Desenvolvimento </h2>
+                    <h2>Gosto De Estudar , Aprender, Fazer Ná Prática e Ver Como As Coisas Funcionam</h2>
                 </div>
                 <Image
                     src={FotoPerfil}
-                    alt="FotoPerfil"
+                    alt="Foto"
                     width={350}
-                    height={350}
+                    height={400}
                 />
             </div>
         </div>
