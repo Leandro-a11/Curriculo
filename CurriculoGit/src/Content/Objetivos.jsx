fad29ea63df3145313e6dc824d554277a843359a
Leandro-a11 <letalinkpark@gmail.com>
2022-08-05 18:39:10 -0300

    Ortografia Minima Alterada

--- CurriculoGit/src/Content/Objetivos.jsx
+++ CurriculoGit/src/Content/Objetivos.jsx
@@ -4,14 +4,13 @@ export default function TextoObjetivos() {
         <div>
             <div className={style.flex}>
                 <div>
-                    <h1 className={style.title}>Bom, Estou a Procura De Emprego Para Desenvolvedor Web FullStack </h1>
+                    <h1 className={style.title}> Estou a Procura De Emprego Para Desenvolvedor Web FullStack </h1>
                     <h2>Estou Dedicando Todo O Meu Tempo Em Adquirir Conhecimentos Nessa Área</h2>
                     <h2>Desde o Meu Primeiro Contato Com o Famoso "Olá Mundo"</h2>
                     <h2>Estou Decidido a Aprender Cada Vez Mais as Habilidades Para Ser o Melhor Desenvolvedor Web </h2>
                     <h2>Visando O Crescimento Profissional e Pessoal Para Contribuir Com o Crescimento Da Minha Empresa</h2>
                     <h1 className={style.title}>Tenho Qualidades Como:</h1>
                     <h2><q>Responsabilidade, Dedicação, Interesse em Aprender, Honestidade, Entre Outros </q></h2>
-                    <h1></h1>
                     <h1>Estou em Busca De Uma Oportunidade </h1>
                     <h1>Para Aplicar Meus Conhecimentos Adquirido Ao Longo Do Tempo</h1>
                 </div>
